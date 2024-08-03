7ddaf0316e82acd8a449c6e118669dd43447ba71
Kelsey <37933054+DandyDev01@users.noreply.github.com>
2024-08-02 22:04:30 -0600

    messed around with invincible settings

--- Assets/Dungeon Shooter/Scripts/Entities/Player/PlayerDodgeState.cs
+++ Assets/Dungeon Shooter/Scripts/Entities/Player/PlayerDodgeState.cs
@@ -12,10 +12,12 @@ namespace DungeonShooter.Player
 	public class PlayerDodgeState : PlayerStateBase
 	{
 		private readonly Rigidbody2D _rigidbody;
+		private readonly Timer _timer;
 
 		public PlayerDodgeState(Player player) : base(player)
 		{
 			_rigidbody = player.GetComponent<Rigidbody2D>();
+			_timer = new Timer(0.5f, false);
 		}
 
 		public override void Enter()
@@ -23,21 +25,27 @@ namespace DungeonShooter.Player
 			_player.PlayAnimation("Dodge");
 			_player.AddEffect(new InvincibleEffect(1f));
 
+			_timer.Play();
+
 			_rigidbody.AddForce(_player.MoveVector * _player.DodgeForce, ForceMode2D.Impulse);
 		}
 
 		public override void Exit()
 		{
+			_timer.Stop();
+			_timer.Reset(0.5f);
 		}
 
 		public override void Run()
 		{
+			_timer.Tick(Time.deltaTime);
 			CheckForStateSwitch();
 		}
 
 		protected override void CheckForStateSwitch()
 		{
-			SwitchState(_player.StateHolder.IdleState);
+			if (_timer.Finished)
+				SwitchState(_player.StateHolder.IdleState);
 		}
 	}
 }
