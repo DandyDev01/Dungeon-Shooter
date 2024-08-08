25f0e5b87a891f45eec95f6a9793da96a8152da7
Kelsey <37933054+DandyDev01@users.noreply.github.com>
2024-08-07 20:34:21 -0600

    added shield interactable

---
+++ Assets/Dungeon Shooter/Scripts/Interactables/ShieldInteractable.cs
@@ -0,0 +1,16 @@
+using DungeonShooter.Player;
+using DungeonShooter.Player.Effects;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DungeonShooter
+{
+	public class ShieldInteractable : InteractableBase
+	{
+		public override void Interact(PlayerCharacter player)
+		{
+			player.AddEffect(new InvincibleEffect(5f));
+		}
+	}
+}
